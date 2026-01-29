717b19e8415e99ededf1532b9560c8cb64edd878
Hem Patel <hempatel777@yahoo.com>
2026-01-29 11:19:26 +0530

    Fix Sequelize initialization to use DATABASE_URL in production

--- server/config/sequelize.js
+++ server/config/sequelize.js
@@ -4,20 +4,21 @@ const config = require('./database.js');
 const env = process.env.NODE_ENV || 'development';
 const dbConfig = config[env];
 
-const sequelize = new Sequelize(
-  dbConfig.database,
-  dbConfig.username,
-  dbConfig.password,
-  {
-    host: dbConfig.host,
-    port: dbConfig.port,
-    dialect: dbConfig.dialect,
-    logging: dbConfig.logging,
-    pool: dbConfig.pool,
-    define: dbConfig.define,
-    dialectOptions: dbConfig.dialectOptions
+let sequelize;
+
+if (dbConfig.use_env_variable) {
+  if (!process.env[dbConfig.use_env_variable]) {
+    throw new Error(`Error: Environment variable ${dbConfig.use_env_variable} is not defined.`);
   }
-);
+  sequelize = new Sequelize(process.env[dbConfig.use_env_variable], dbConfig);
+} else {
+  sequelize = new Sequelize(
+    dbConfig.database,
+    dbConfig.username,
+    dbConfig.password,
+    dbConfig
+  );
+}
 
 // Test the connection
 const testConnection = async () => {
