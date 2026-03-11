4bf041e08ca80bd04883d8a8b5e960c1ad0729bf
AadityaMahil <aadityamahil1409@gmail.com>
2026-03-11 12:42:48 +0530

    fix: disable logging in database configuration

--- server/config/database.js
+++ server/config/database.js
@@ -35,7 +35,7 @@ module.exports = {
     host: process.env.DB_HOST || "localhost",
     port: process.env.DB_PORT || 5432,
     dialect: "postgres",
-    logging: console.log,
+    logging: false,
     pool: {
       max: 5,
       min: 0,
